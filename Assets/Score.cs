a3a4b0ef8433e63d24bbcb26a1b48abd0029b68a
npagan201 <npagan@rams.colostate.edu>
2020-05-10 06:28:06 -0500

    3D-Audio Project
    Full Final Project

---
+++ Assets/Score.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using System.IO;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Score : MonoBehaviour
+{
+    public int score;
+    public float timePassed;
+    GameObject[] player; 
+
+    void Start()
+    {
+        score = 0;
+        timePassed = 0;
+        player = GameObject.FindGameObjectsWithTag("MainCamera");
+        player[0].GetComponent<Light>().enabled = false;
+    }
+
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1);
+    }
+
+    public void increaseScore()
+    {
+        score++;
+        StreamWriter writer = new StreamWriter("./times.txt", true);
+        writer.WriteLine("Sphere" + (score-1).ToString() + ": " + (timePassed).ToString());
+        writer.Close();
+        timePassed = 0;
+
+        if (score == 4)
+        {
+            wait();
+            transform.position = new Vector3(705, 3, 1300);
+            wait();
+        }
+
+        if (score == 8)
+        {
+            wait();
+            transform.position = new Vector3(-527, 3, 477);
+            wait();
+            player[0].GetComponent<Light>().enabled = true;
+        }
+
+        if (score == 12)
+        {
+            wait();   
+            wait();
+            wait();
+            wait();
+            wait();
+            Application.Quit();
+        }
+
+        
+
+    }
+
+}
---
+++ Assets/SphereCollect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using UnityEngine;
+
+public class SphereCollect : MonoBehaviour
+{
+    public GameObject[] allSpheres;
+    public int currSphere;
+
+    void Start()
+    {
+        allSpheres = new GameObject[11];
+        currSphere = 0;
+
+        GameObject[] sphere1 = GameObject.FindGameObjectsWithTag("Sphere1");
+        GameObject[] sphere2 = GameObject.FindGameObjectsWithTag("Sphere2");
+        GameObject[] sphere3 = GameObject.FindGameObjectsWithTag("Sphere3");
+        GameObject[] sphere4 = GameObject.FindGameObjectsWithTag("Sphere4");
+        GameObject[] sphere5 = GameObject.FindGameObjectsWithTag("Sphere5");
+        GameObject[] sphere6 = GameObject.FindGameObjectsWithTag("Sphere6");
+        GameObject[] sphere7 = GameObject.FindGameObjectsWithTag("Sphere7");
+        GameObject[] sphere8 = GameObject.FindGameObjectsWithTag("Sphere8");
+        GameObject[] sphere9 = GameObject.FindGameObjectsWithTag("Sphere9");
+        GameObject[] sphere10 = GameObject.FindGameObjectsWithTag("Sphere10");
+        GameObject[] sphere11 = GameObject.FindGameObjectsWithTag("Sphere11");
+        
+        allSpheres[0] = sphere1[0];
+        allSpheres[1] = sphere2[0];
+        allSpheres[2] = sphere3[0];
+        allSpheres[3] = sphere4[0];
+        allSpheres[4] = sphere5[0];
+        allSpheres[5] = sphere6[0];
+        allSpheres[6] = sphere7[0];
+        allSpheres[7] = sphere8[0];
+        allSpheres[8] = sphere9[0];
+        allSpheres[9] = sphere10[0];
+        allSpheres[10] = sphere11[0];
+
+        foreach(GameObject sphere in allSpheres)
+        {
+            sphere.SetActive(false);
+        }
+       
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Sphere" + currSphere.ToString()));
+        {
+            col.gameObject.SetActive(false);
+            gameObject.GetComponent<Score>().increaseScore();
+            
+            if (currSphere < 11)
+            {
+                allSpheres[currSphere].SetActive(true);
+            }
+
+            currSphere++;
+        }
+    }
+}
